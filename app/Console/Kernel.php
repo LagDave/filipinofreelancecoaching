ae188014d9596861f193fbf532a449ec517c4259
rdavephp <rdavephp@gmail.com>
2020-04-23 08:50:34 +0800

    Final setup for cronjob

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('command:manageExpiredUsers')->everyMinute();
+        $schedule->command('command:manageExpiredUsers')->daily();
     }
 
     /**
