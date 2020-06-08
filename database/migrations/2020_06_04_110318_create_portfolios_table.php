0ea7cf0bdf006869a81f9a37e8aa291992216e95
rdavephp <rdavephp@gmail.com>
2020-06-08 22:32:58 +0800

    unified portfolios column migration

--- database/migrations/2020_06_04_110318_create_portfolios_table.php
+++ database/migrations/2020_06_04_110318_create_portfolios_table.php
@@ -19,6 +19,19 @@ class CreatePortfoliosTable extends Migration
             $table->unsignedInteger('user_id');
             $table->string('username');
             $table->string('theme')->default('litera');
+
+            $table->string('full_name');
+            $table->string('role')->default("FFC Student");
+            $table->string('facebook_link')->nullable();
+            $table->string('twitter_link')->nullable();
+            $table->string('linkedin_link')->nullable();
+            $table->string('instagram_link')->nullable();
+            $table->string('resume_link')->nullable();
+            $table->string('profile_pic')->default('/portfolio-images/dp.png');
+            $table->text('about_content')->default('Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure dolorum dolorem itaque odit earum dolor vitae architecto ea asperiores ab! Voluptatem, nam! Quo quisquam perferendis sit et error facilis eveniet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, dignissimos commodi? Modi sapiente dolorem reprehenderit architecto sint dolore, nostrum inventore consequuntur vel odit, explicabo tenetur. Sunt possimus facere id nemo. Lorem ipsum dolor sit amet consectetur adipisici.');
+            $table->string('address')->default('Bldg. 21, Sunflower st., Barangay Matina, Davao City');
+            $table->string('phone_number')->default('0910xxxxxxx');
+            $table->string('email');
         });
     }
 
