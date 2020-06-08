d3d6c72fe4bd08645eb8961ba1e46a81aef29f6a
rdavephp <rdavephp@gmail.com>
2020-06-08 22:42:42 +0800

    migration fix;

--- database/migrations/2020_06_04_110318_create_portfolios_table.php
+++ database/migrations/2020_06_04_110318_create_portfolios_table.php
@@ -28,7 +28,7 @@ class CreatePortfoliosTable extends Migration
             $table->string('instagram_link')->nullable();
             $table->string('resume_link')->nullable();
             $table->string('profile_pic')->default('/portfolio-images/dp.png');
-            $table->text('about_content')->default('Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure dolorum dolorem itaque odit earum dolor vitae architecto ea asperiores ab! Voluptatem, nam! Quo quisquam perferendis sit et error facilis eveniet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, dignissimos commodi? Modi sapiente dolorem reprehenderit architecto sint dolore, nostrum inventore consequuntur vel odit, explicabo tenetur. Sunt possimus facere id nemo. Lorem ipsum dolor sit amet consectetur adipisici.');
+            $table->longText('about_content')->nullable()->default('Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure dolorum dolorem itaque odit earum dolor vitae architecto ea asperiores ab! Voluptatem, nam! Quo quisquam perferendis sit et error facilis eveniet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, dignissimos commodi? Modi sapiente dolorem reprehenderit architecto sint dolore, nostrum inventore consequuntur vel odit, explicabo tenetur. Sunt possimus facere id nemo. Lorem ipsum dolor sit amet consectetur adipisici.');
             $table->string('address')->default('Bldg. 21, Sunflower st., Barangay Matina, Davao City');
             $table->string('phone_number')->default('0910xxxxxxx');
             $table->string('email');
