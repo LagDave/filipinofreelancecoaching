3932c0674610cac3447fb89d91597fcd41833be0
LagDave <laggy80@gmail.com>
2020-08-14 09:57:51 +0800

    updates

--- resources/views/users/enroll_page.blade.php
+++ resources/views/users/enroll_page.blade.php
@@ -15,19 +15,15 @@
 <section id="pricing" class="enroll-page-pricing pricing">
   <div class="container">
 
-    <div class="row">
+    <div class="row justify-content-center">
 
       <div class="col-lg-4 col-md-6">
         <div class="box" data-aos="zoom-in-right" data-aos-delay="200">
           <h3>Yearly</h3>
-          <h4><sup>₱</sup>999<span> / year</span></h4>
+          <h4><sup>$</sup>10</h4>
           <ul>
             <li>100+ Courses</li>
             <small><li>Web Portfolio</li></small>
-            <li>Templates and Checklists</li>
-            <li>Softwares and Tools</li>
-            <li>E-Books</li>
-            <li>Cheat sheet</li>
             <li>Certificates</li>
             <li>Chat support</li>
           </ul>
@@ -37,37 +33,13 @@
         </div>
       </div>
 
-      <div class="col-lg-4 col-md-6 mt-4 mt-md-0">
-        <div class="box recommended" data-aos="zoom-in" data-aos-delay="100">
-          <h3>Monthly</h3>
-          <h4><sup>₱</sup>399<span> / month</span></h4>
-          <ul>
-            <li>100+ Courses</li>
-            <small><li>Web Portfolio</li></small>
-            <li>Templates and Checklists</li>
-            <li>Softwares and Tools</li>
-            <li>E-Books</li>
-            <li>Cheat sheet</li>
-            <li>Certificates</li>
-            <li class="na">Chat support</li>
-          </ul>
-          <div class="btn-wrap">
-            <a href="/home/enroll/monthly/basic_info" class="btn-buy">Enroll Now</a>
-          </div>
-        </div>
-      </div>
-
       <div class="col-lg-4 col-md-6 mt-4 mt-lg-0">
         <div class="box" data-aos="zoom-in-left" data-aos-delay="200">
           <h3>Lifetime</h3>
-          <h4><sup>₱</sup>1,999<span> / limitless</span></h4>
+          <h4><sup>$</sup>29<span></span></h4>
           <ul>
             <li>100+ Courses</li>
             <small><li>Web Portfolio</li></small>
-            <li>Templates and Checklists</li>
-            <li>Softwares and Tools</li>
-            <li>E-Books</li>
-            <li>Cheat sheet</li>
             <li>Certificates</li>
             <li>Chat support</li>
           </ul>
